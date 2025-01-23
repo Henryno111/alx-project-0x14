29de6ec34a7bb1157a0e5e2f0c2c4576e778bc0b
henryno111 <h.agukwe@yahoo.com>
2025-01-23 11:01:36 +0100

    changed component files

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { ButtonProps } from "@/interfaces";
 
-
-
-const Button: React.FC = () => {
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
-      <button>Button</button>
+    <button
+      onClick={action}
+      className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300"
+    >
+      {title}
+    </button>
   );
 };
 
